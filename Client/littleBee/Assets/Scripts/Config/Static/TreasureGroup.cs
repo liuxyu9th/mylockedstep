19b743e7880605de8e496099915669b3be5e91dd
Liux <2244695293@qq.com>
2025-03-21 14:45:23 +0800

    配置工具

---
+++ Client/littleBee/Assets/Scripts/Config/Static/TreasureGroup.cs
@@ -0,0 +1,12 @@
+﻿using Synchronize.Game.Lockstep.Config.Static.Interface;
+
+namespace Synchronize.Game.Lockstep.Config.Static
+{
+    public class TreasureGroup:ICFG
+    {
+        public int ConfigId { get; set; }
+        public int GroupId{ get; set; }
+        public int TreasureId { get; set; }
+        public int Prob { get; set; }
+    }
+}
